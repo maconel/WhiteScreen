2dd3a91aeca052f3c019a05f89599f9160f7a09a
redyan <redyan@tencent.com>
2022-10-08 20:36:47 +0800

    完成

---
+++ WhiteScreen/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WhiteScreen
+//
+//  Created by maconelsun on 2022/10/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+}
